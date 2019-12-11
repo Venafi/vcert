fa2276d8283a3a0c85dbf48d030b42d9aa5b238c
Denis Subbotin <denis@subbot.in>
2019-12-11 18:48:51 +0300

    check certificate in tests

--- listener_test.go
+++ listener_test.go
@@ -1,8 +1,11 @@
 package vcert
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"github.com/Venafi/vcert/pkg/endpoint"
+	"github.com/Venafi/vcert/pkg/venafi/fake"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -12,20 +15,23 @@ import (
 func TestConfig_NewListener(t *testing.T) {
 
 	t.Run("normal", func(t *testing.T) {
-		testListener(t, "18443", []string{"test.example.com:18443"}, true)
+		testListener(t, "localhost:18443", []string{"localhost:18443"}, true)
 	})
 	t.Run("default port", func(t *testing.T) {
-		testListener(t, "443", []string{"example.com"}, true)
+		testListener(t, "localhost:443", []string{"localhost"}, true)
 	})
 	t.Run("two domains", func(t *testing.T) {
-		testListener(t, "8443", []string{"example.com", "test.example.com:8443"}, true)
+		testListener(t, "localhost:8443", []string{"localhost", "test.example.com:8443"}, true)
 	})
 	t.Run("port conflict", func(t *testing.T) {
-		testListener(t, "8444", []string{"example.com:443", "test.example.com:8444"}, false)
+		testListener(t, "localhost:8444", []string{"localhost:443", "test.example.com:8444"}, false)
+	})
+	t.Run("invalid hostname", func(t *testing.T) {
+		testListener(t, "localhost:8445", []string{"example.com:8445"}, false)
 	})
 }
 
-func testListener(t *testing.T, port string, domains []string, success bool) {
+func testListener(t *testing.T, host string, domains []string, success bool) {
 	const text = "It works!\n"
 	cfg := Config{ConnectorType: endpoint.ConnectorTypeFake}
 	mux := http.NewServeMux()
@@ -37,8 +43,15 @@ func testListener(t *testing.T, port string, domains []string, success bool) {
 	defer listener.Close()
 	go http.Serve(listener, mux)
 	time.Sleep(time.Millisecond * 100)
-
-	r, err := http.Get("https://localhost:" + port + "/")
+	client := http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{}}}
+	_, err := client.Get("https://" + host + "/")
+	if err == nil {
+		t.Fatal("without trust bundle connection should fail")
+	}
+	connectionTrustBundle := x509.NewCertPool()
+	connectionTrustBundle.AppendCertsFromPEM([]byte(fake.CaCertPEM))
+	client = http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{RootCAs: connectionTrustBundle}}}
+	r, err := client.Get("https://" + host + "/")
 	//todo: custom client and check server certificate
 	if success && err != nil {
 		t.Fatal(err)
