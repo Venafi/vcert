b54d8101f19351609e867a63227100b00fd12a07
marcos-albornoz <marcos.albornoz@venafi.com>
2022-04-26 18:25:51 -0500

    Re-disable cloud/connector_test.TestGetPolicy

--- pkg/venafi/cloud/connector_test.go
+++ pkg/venafi/cloud/connector_test.go
@@ -1087,7 +1087,7 @@ func TestSetPolicy(t *testing.T) {
 
 func TestGetPolicy(t *testing.T) {
 
-	//t.Skip() //this is just for development purpose
+	t.Skip() //this is just for development purpose
 
 	policyName := os.Getenv("CLOUD_POLICY_MANAGEMENT_SAMPLE")
 	conn := getTestConnector(ctx.CloudZone)
