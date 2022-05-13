370e11fd70a410459e39a72d257945753cf89dfb
marcos-albornoz <marcos.albornoz@venafi.com>
2022-05-13 16:29:43 -0500

    Update getcred help description

--- cmd/vcert/main.go
+++ cmd/vcert/main.go
@@ -105,7 +105,7 @@ ACTIONS:
    getpolicy    To retrieve the certificate policy of a zone
    setpolicy    To apply a certificate policy specification to a zone
 
-   getcred      To obtain a new token for authentication on TPP or create a user account on VaaS
+   getcred      To obtain a new TPP authentication token or register for a new VaaS user API key
    checkcred    To check the validity of a token and grant
    voidcred     To invalidate an authentication grant
 
