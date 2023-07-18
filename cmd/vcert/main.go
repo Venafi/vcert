2195b4df4cfa50e415f70ebd2c817e67dd3f0180
Russel Vela <russel.vela@venafi.com>
2023-07-18 12:39:13 -0600

    feat(playbook): Added playbook feature to cli
    Added ability to run playbooks from vcert cli
    
    Closes VC-24595

--- cmd/vcert/main.go
+++ cmd/vcert/main.go
@@ -23,8 +23,9 @@ import (
 	"sort"
 	"time"
 
-	"github.com/Venafi/vcert/v4"
 	"github.com/urfave/cli/v2"
+
+	"github.com/Venafi/vcert/v4"
 )
 
 var (
@@ -76,9 +77,10 @@ func main() {
 			commandSshPickup,
 			commandSshEnroll,
 			commandSshGetConfig,
+			commandRunPlaybook,
 		},
 		EnableBashCompletion: true, //todo: write BashComplete function for options
-		//HideHelp:             true,
+		Authors:              authors,
 		Copyright: `2018-2022 Venafi, Inc.
 	 Licensed under the Apache License, Version 2.0`,
 	}
@@ -93,16 +95,16 @@ USAGE:
    {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
    {{if len .Authors}}
 AUTHOR:
-   {{range .Authors}}{{ . }}{{end}}
-   {{end}}{{if .Commands}}
+   {{range .Authors}}{{ . }}
+   {{end}}{{end}}{{if .Commands}}
 ACTIONS:
-
    gencsr       To generate a certificate signing request (CSR)
    enroll       To enroll a certificate
    pickup       To retrieve a certificate
    renew        To renew a certificate
-   revoke       To revoke a certificate
    retire       To retire a certificate
+   revoke       To revoke a certificate
+   run          To retrieve and install certificates using a vcert playbook file
 
    getpolicy    To retrieve the certificate policy of a zone
    setpolicy    To apply a certificate policy specification to a zone
@@ -123,7 +125,7 @@ COPYRIGHT:
    {{end}}{{if .Version}}
 SUPPORT:
    opensource@venafi.com
-   {{end}}
+{{end}}
 `, vcert.GetFormattedVersionString(), vcert.GetFormatedBuildTimeStamp())
 
 	cli.CommandHelpTemplate = `NAME:
@@ -148,4 +150,23 @@ OPTIONS:
 		logger := log.New(os.Stderr, UtilityShortName+": ", log.LstdFlags)
 		logger.Panicf("%s", err)
 	}
+}
+
+var authors = []*cli.Author{
+	{
+		Name:  "Ryan Treat",
+		Email: "ryan.treat@venafi.com",
+	},
+	{
+		Name:  "Russel Vela",
+		Email: "russel.vela@venafi.com",
+	},
+	{
+		Name:  "Luis Presuel",
+		Email: "luis.presuel@venafi.com",
+	},
+	{
+		Name:  "Marcos Albornoz",
+		Email: "marcos.albornoz@venafi.com",
+	},
 }
--- go.mod
+++ go.mod
@@ -1,22 +1,44 @@
 module github.com/Venafi/vcert/v4
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/howeyc/gopass v0.0.0-20170109162249-bf9dde6d0d2c
 	github.com/pavel-v-chernykh/keystore-go/v4 v4.1.0
+	github.com/pkg/errors v0.8.1
 	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d
+	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.7.0
+	github.com/stretchr/testify v1.8.1
 	github.com/urfave/cli/v2 v2.1.1
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	go.uber.org/zap v1.23.0
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
 	gopkg.in/ini.v1 v1.51.0
 	gopkg.in/yaml.v2 v2.4.0
-	software.sslmate.com/src/go-pkcs12 v0.2.0
 	gopkg.in/yaml.v3 v3.0.1
-	go.uber.org/zap v1.23.0
-	github.com/stretchr/testify v1.8.1
-	github.com/google/uuid v1.3.0
-	github.com/spf13/cobra v1.6.1
+	software.sslmate.com/src/go-pkcs12 v0.2.0
 
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
 
 go 1.20
