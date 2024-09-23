b9295969db0f5adaf50c68dbcdf5219383d1fe3f
Luis Presuel <luis.presuel@venafi.com>
2024-09-23 12:23:06 -0600

    fixes goimports

--- pkg/venafi/cloud/cloudUtil.go
+++ pkg/venafi/cloud/cloudUtil.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/Venafi/vcert/v5/pkg/certificate"
-	"github.com/Venafi/vcert/v5/pkg/util"
 	"net/http"
 	"regexp"
+
+	"github.com/Venafi/vcert/v5/pkg/certificate"
+	"github.com/Venafi/vcert/v5/pkg/util"
 )
 
 func parseDEKInfo(httpStatusCode int, httpStatus string, body []byte) (*EdgeEncryptionKey, error) {
