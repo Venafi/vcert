13794ad7d8b2e04b100da44b54cd0909efb79f8d
marcos-albornoz <marcos.albornoz@venafi.com>
2025-07-20 18:13:52 -0600

    Impr: Add services interfaces for testing
    closes #573

--- pkg/webclient/caoperations/caoperations.go
+++ pkg/webclient/caoperations/caoperations.go
@@ -5,20 +5,36 @@ import (
 	"net/http"
 
 	"github.com/Khan/genqlient/graphql"
+
+	"github.com/Venafi/vcert/v5/pkg/webclient/caoperations/service"
 )
 
 //go:generate go run -mod=mod github.com/Khan/genqlient genqlient.yaml
 
 type CAOperationsClient struct {
-	graphqlClient graphql.Client
+	caOperationsService service.CAOperationsServiceWrapper
 }
 
 func NewCAOperationsClient(url string, httpClient *http.Client) *CAOperationsClient {
 	return &CAOperationsClient{
-		graphqlClient: graphql.NewClient(url, httpClient),
+		caOperationsService: &CAOperationsService{
+			graphqlClient: graphql.NewClient(url, httpClient),
+		},
 	}
 }
 
-func (c *CAOperationsClient) RevokeCertificate(ctx context.Context, fingerprint string, certificateAuthorityAccountId *string, revocationReason RevocationReason, revocationComment *string) (*RevokeCertificateRequestResponse, error) {
-	return RevokeCertificateRequest(ctx, c.graphqlClient, fingerprint, certificateAuthorityAccountId, revocationReason, revocationComment)
+func (c *CAOperationsClient) SetCAOperationsService(caOperationsService service.CAOperationsServiceWrapper) {
+	c.caOperationsService = caOperationsService
+}
+
+func (c *CAOperationsClient) RevokeCertificate(ctx context.Context, fingerprint string, certificateAuthorityAccountId *string, revocationReason service.RevocationReason, revocationComment *string) (*service.RevokeCertificateRequestResponse, error) {
+	return c.caOperationsService.RevokeCertificateRequest(ctx, fingerprint, certificateAuthorityAccountId, revocationReason, revocationComment)
+}
+
+type CAOperationsService struct {
+	graphqlClient graphql.Client
+}
+
+func (ca *CAOperationsService) RevokeCertificateRequest(ctx context.Context, fingerprint string, certificateAuthorityAccountId *string, revocationReason service.RevocationReason, revocationComment *string) (*service.RevokeCertificateRequestResponse, error) {
+	return service.RevokeCertificateRequest(ctx, ca.graphqlClient, fingerprint, certificateAuthorityAccountId, revocationReason, revocationComment)
 }
